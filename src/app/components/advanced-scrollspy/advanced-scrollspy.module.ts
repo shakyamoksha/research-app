c88184418c3ecde7c4642ce9096941cfc56f209c
deesoo <deeptish.sookun@mcb.mu>
2021-09-17 18:30:01 +0400

    added number formatting directive

--- src/app/components/advanced-scrollspy/advanced-scrollspy.module.ts
+++ src/app/components/advanced-scrollspy/advanced-scrollspy.module.ts
@@ -5,11 +5,12 @@ import { AdvancedScrollspyRoutingModule } from './advanced-scrollspy-routing.mod
 import { AdvancedScrollspyComponent } from './components/advanced-scrollspy.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {MaterialModule} from '../../shared/material-design.module';
+import {LocalizedNumericInputDirective} from '../../directives/LocalizedNumericInput.directive';
 
 
 
 @NgModule({
-  declarations: [AdvancedScrollspyComponent],
+    declarations: [AdvancedScrollspyComponent, LocalizedNumericInputDirective],
   imports: [
     CommonModule,
     AdvancedScrollspyRoutingModule,
