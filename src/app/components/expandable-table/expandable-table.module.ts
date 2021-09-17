0fb0d5c9f6b1ac1045306fd39b3bd2ee09dfd109
deesoo <deeptish.sookun@mcb.mu>
2021-09-17 15:37:58 +0400

    temporary commit

--- src/app/components/expandable-table/expandable-table.module.ts
+++ src/app/components/expandable-table/expandable-table.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { ExpandableTableRoutingModule } from './expandable-table-routing.module';
 import {ExpandableTableComponent} from './components/expandable-table.component';
+import {MatTableModule} from '@angular/material/table';
 
 
 @NgModule({
   declarations: [ExpandableTableComponent],
-  imports: [
-    CommonModule,
-    ExpandableTableRoutingModule
-  ]
+    imports: [
+        CommonModule,
+        ExpandableTableRoutingModule,
+        MatTableModule
+    ]
 })
 export class ExpandableTableModule { }
